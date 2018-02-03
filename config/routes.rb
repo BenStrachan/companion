912a85ec0efa2437bbc0400e42466ae296ea7ab3
Ben Strachan <ben@ownerhealth.com.au>
2018-02-03 19:20:59 +1000

    2 commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root to: 'visitors#index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    invitations: 'users/invitations',
+  }
+
+  namespace :admin do
+    resources :users
+  end
+end
