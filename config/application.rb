646e2f88092f9fb5b9b90f9a3ba68685c3d4ee79
chutuan <chutuan020693@gmail.com>
2018-02-04 14:03:14 +0700

    #2 update assets

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Template
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.assets.serve_static_files = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
