4d8ac4b72121e77641dd995aded6e689048a73a1
Ben Strachan <ben@ownerhealth.com.au>
2018-02-25 19:11:08 +1000

    settings and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def after_sign_in_path_for(resource)
-      request.env['omniauth.origin'] || stored_location_for(resource) || dashboards_index_path
+      request.env['omniauth.origin'] || stored_location_for(resource) || app_advertisements_path
     end
 
       # Overwriting the sign_out redirect path method
